3bf1e380221e0ebf14a5812a7769fc9880271ad6
Sushmitha K <ksusmitha36@gmail.com>
2026-06-07 15:59:21 +0530

    Created dashboard base structure

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+
+      <section className="p-5">
+        <h2>Dashboard Home</h2>
+      </section>
+    </main>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="p-4 border-b">
+      <h1>Analytics Dashboard</h1>
+    </nav>
+  );
+}
